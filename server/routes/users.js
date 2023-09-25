6395997e0f13ba467d164ad56b29067abf22a3fe
Diego Batista <batistadiego01@gmail.com>
2023-09-25 00:32:33 -0400

    updated to create next app

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,5 @@
 var express = require('express');
-const db = require('../db/db');
+const db = require('../../db/db');
 var router = express.Router();
 
 
@@ -15,15 +15,5 @@ router.get('/getUsers', async(req, res) => {
 
 });
 
-router.get('/get', async(req, res) => {
-    try {
-        var result = await db.query('SELECT * FROM users');
-        res.send(result.rows);
-    }
-    catch {
-        res.status(400).json("Unsuccessful Request");
-    }
-
-});
 
 module.exports = router;
