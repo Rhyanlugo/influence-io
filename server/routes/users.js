af033a0d8a8895b24513c39dc8751ffbbf977076
Diego Batista <batistadiego01@gmail.com>
2023-09-24 21:16:36 -0400

    moved and updated .gitignore, added db connection, added db test to get all users

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,29 @@
 var express = require('express');
+const db = require('../db/db');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+/* GET users async req. */
+router.get('/getUsers', async(req, res) => {
+    try {
+        var result = await db.query('SELECT * FROM users');
+        res.send(result.rows);
+    }
+    catch {
+        res.status(400).json("Unsuccessful Request");
+    }
+
+});
+
+router.get('/get', async(req, res) => {
+    try {
+        var result = await db.query('SELECT * FROM users');
+        res.send(result.rows);
+    }
+    catch {
+        res.status(400).json("Unsuccessful Request");
+    }
+
 });
 
 module.exports = router;
